afc19f034dd0c7729c1236fa014f3d372cc9fab4
Abhishek Rawat <abhiryl420@gmail.com>
2025-11-04 10:54:20 +0530

    hot fix

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -41,7 +41,7 @@ const About = () => {
           <p>
             I'm currently looking for a Full Stack Development Role{" "}
             <a
-              href="https://drive.google.com/file/d/18YUmrCpld_qzezDfMwvWMIQY652zdO_E/view?usp=sharing"
+              href="https://drive.google.com/file/d/1GJHA78xmonh2XIV_g3nhrDyfvbam0vjW/view?usp=sharing"
               target="_blank"
               className="app__links"
             >
